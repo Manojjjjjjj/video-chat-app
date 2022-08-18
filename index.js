a0b78e75da8f76dc3f9b2fd7233413bf599cbb32
manojjjjjjj <mjsahu1999@gmail.com>
2022-08-18 22:32:25 +0530

    changed

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ io.on("connection", (socket) => {
     });
   });
 });
-
-server.listen(3000);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
